7e57c7cfa1bb50adbda23cf4604c6c85fa5ba26e
Mar <mar@coconauts.net>
2025-07-15 13:32:07 +0200

    Add ref to docs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// See specific instructions for github pages here: 
+// https://vite.dev/guide/static-deploy.html#github-pages
 export default defineConfig({
   plugins: [react()],
   base: '/pkmn-typechart-app/',
